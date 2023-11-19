4928c5266d2b866ff50ff1a58e6478a86d82dcb7
Hazem <h.hussien@singularity.finance>
2023-11-19 12:15:32 +0200

    inito todo info

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,33 @@
-import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import './editor.scss';
-
+import { useSelect, useDispatch } from '@wordpress/data';
+import { CheckboxControl, TextControl, Button } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 export default function Edit() {
-	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
-	);
+  const data = useSelect((select) => {
+    const store = select('block-course/todos');
+    return {
+      total: store?.getTodosNumber(),
+      done: store?.getDoneTodos(),
+      undone: store?.getUnDoneTodos(),
+    };
+  }, []);
+  console.log(data);
+  const actions = useDispatch('block-course/todos');
+  const addTodo = actions && actions.addTodo;
+  const toggleTodo = actions && actions.toggleTodo;
+
+  return (
+    <div {...useBlockProps()}>
+      {!data && <p>Pleas make sure your plugin is activated</p>}
+
+      {data && (
+        <ul>
+          <li>Done: {data.done}</li>
+          <li>undone: {data.undone}</li>
+          <li>total: {data.total}</li>
+        </ul>
+      )}
+    </div>
+  );
 }
